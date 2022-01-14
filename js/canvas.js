6eed0c5f08e440b0c5d8ae27ab910e20b32147fe
jaesukpark77 <jaesukpark77@naver.com>
2022-01-14 09:34:04 +0900

    CANVAS 수업 내용
    canvas js 내용 추가

--- js/canvas.js
+++ js/canvas.js
@@ -1,12 +1,12 @@
 const canvas1 = document.getElementById('canvas1');
 const ctx1 = canvas1.getContext('2d');
-ctx1.fillStyle = 'chocolate'
+ctx1.fillStyle = 'chocolate';
 ctx1.fillRect(150,100,300,200);
 
 const canvas2 = document.getElementById('canvas2');
 const ctx2 = canvas2.getContext('2d');
 ctx2.moveTo(0,0);ctx2.lineTo(600,400);
-ctx2.strokeStyle = 'cornflowerblue'
+ctx2.strokeStyle = 'cornflowerblue';
 ctx2.stroke();
 
 const canvas3 = document.getElementById('canvas3');
@@ -14,4 +14,45 @@ const ctx3 = canvas3.getContext('2d');
 ctx3.moveTo(300,50); ctx3.lineTo(50,350); ctx3.lineTo(550,350); ctx3.lineTo(300,50);
 // ctx3.stroke(); -> 확인하기 위해서
 ctx3.fillStyle = 'coral';
-ctx3.fill()
+ctx3.fill();
+
+const canvas4 = document.getElementById('canvas4');
+const ctx4 = canvas4.getContext('2d');
+ctx4.shadowColor = 'black';
+ctx4.shadowBlur = 50;
+ctx4.shadowOffsetX = 20;
+ctx4.shadowOffsetY = 30;
+ctx4.fillStyle = 'cornflowerblue';
+ctx4.fillRect(150,100,300,200);
+
+const canvas5 = document.getElementById('canvas5');
+const ctx5 = canvas5.getContext('2d');
+ctx5.fillStyle='cornflowerblue';
+ctx5.fillRect(150,100,200,100);
+ctx5.globalAlpha = .8;
+ctx5.fillStyle='coral';
+ctx5.fillRect(200,150,200,100);
+ctx5.globalAlpha =1;
+ctx5.fillStyle='black';
+ctx5.fillRect(250,200,200,100);
+
+const canvas6 = document.getElementById('canvas6');
+const ctx6 = canvas6.getContext('2d');
+const gra1 = ctx6.createLinearGradient(0,0,canvas6.width,canvas6.height);
+gra1.addColorStop(0, 'red');
+gra1.addColorStop(0.5, 'white');
+gra1.addColorStop(1, 'blue');
+ctx6.fillStyle = gra1;
+ctx6.fillRect(150,100,300,200);
+// ctx6.fillRect(0,0,300,200);
+// ctx6.fillRect(300,200,300,200);
+
+const canvas7 = document.getElementById('canvas7');
+const ctx7 = canvas7.getContext('2d');
+const gra2 = ctx7.createLinearGradient(0,0,0,canvas7.height);
+gra2.addColorStop(0, '#FDD5C0');
+gra2.addColorStop(0.6, '#F46161');
+gra2.addColorStop(1, '#FFCF62');
+
+ctx7.fillStyle = gra2;
+ctx7.fillRect(0,0,canvas7.width,canvas7.height);

df1a17ec7956b4543243034ebda5bd9da3c1c423
jaesukpark77 <jaesukpark77@naver.com>
2022-01-24 21:33:41 +0900

    CANVAS 수업 내용
    js 내용 코드 추가

--- js/canvas.js
+++ js/canvas.js
@@ -1,14 +1,17 @@
+// 1.
 const canvas1 = document.getElementById('canvas1');
 const ctx1 = canvas1.getContext('2d');
 ctx1.fillStyle = 'chocolate';
 ctx1.fillRect(150,100,300,200);
 
+// 2.
 const canvas2 = document.getElementById('canvas2');
 const ctx2 = canvas2.getContext('2d');
 ctx2.moveTo(0,0);ctx2.lineTo(600,400);
 ctx2.strokeStyle = 'cornflowerblue';
 ctx2.stroke();
 
+// 3.
 const canvas3 = document.getElementById('canvas3');
 const ctx3 = canvas3.getContext('2d');
 ctx3.moveTo(300,50); ctx3.lineTo(50,350); ctx3.lineTo(550,350); ctx3.lineTo(300,50);
@@ -16,6 +19,7 @@ ctx3.moveTo(300,50); ctx3.lineTo(50,350); ctx3.lineTo(550,350); ctx3.lineTo(300,
 ctx3.fillStyle = 'coral';
 ctx3.fill();
 
+// 4.
 const canvas4 = document.getElementById('canvas4');
 const ctx4 = canvas4.getContext('2d');
 ctx4.shadowColor = 'black';
@@ -25,6 +29,7 @@ ctx4.shadowOffsetY = 30;
 ctx4.fillStyle = 'cornflowerblue';
 ctx4.fillRect(150,100,300,200);
 
+// 5.
 const canvas5 = document.getElementById('canvas5');
 const ctx5 = canvas5.getContext('2d');
 ctx5.fillStyle='cornflowerblue';
@@ -36,6 +41,7 @@ ctx5.globalAlpha =1;
 ctx5.fillStyle='black';
 ctx5.fillRect(250,200,200,100);
 
+// 6.
 const canvas6 = document.getElementById('canvas6');
 const ctx6 = canvas6.getContext('2d');
 const gra1 = ctx6.createLinearGradient(0,0,canvas6.width,canvas6.height);
@@ -47,6 +53,7 @@ ctx6.fillRect(150,100,300,200);
 // ctx6.fillRect(0,0,300,200);
 // ctx6.fillRect(300,200,300,200);
 
+// 7.
 const canvas7 = document.getElementById('canvas7');
 const ctx7 = canvas7.getContext('2d');
 const gra2 = ctx7.createLinearGradient(0,0,0,canvas7.height);
@@ -59,6 +66,7 @@ ctx7.strokeStyle = gra1;
 ctx7.shadowColor = 'yellow'; ctx7.shadowBlur = 30;
 ctx7.strokeRect(150,100,300,200);
 
+// 8.
 const canvas8 = document.getElementById('canvas8');
 const ctx8 = canvas8.getContext('2d');
 const gra3 = ctx8.createRadialGradient(200,150,5,300,200,300);
@@ -71,6 +79,7 @@ gra3.addColorStop(1, '#5F86FA');
 ctx8.fillStyle = gra3;
 ctx8.fillRect(0,0,canvas8.width,canvas8.height);
 
+// 9.
 const canvas9 = document.getElementById('canvas9');
 const ctx9 = canvas9.getContext('2d');
 const patImg1 = document.getElementById('pat1');
@@ -78,6 +87,7 @@ const myPat1 = ctx9.createPattern(patImg1, 'repeat');
 ctx9.fillStyle = myPat1;
 ctx9.fillRect(150,100,300,200);
 
+// 10.
 const canvas10 = document.getElementById('canvas10');
 const ctx10 = canvas10.getContext('2d');
 const myImg1 = new Image();
@@ -88,6 +98,7 @@ myImg1.onload = function(){
     ctx10.fillRect(0,0,canvas10.width,canvas10.height);
 }
 
+// 11.
 const canvas11 = document.getElementById('canvas11');
 const ctx11 = canvas11.getContext('2d'); 
 const gra4 = ctx11.createLinearGradient(0,0,0,canvas11.height);
@@ -113,6 +124,7 @@ myImg2.onload = function(){
     ctx11.fill();
 }
 
+// 12.
 const canvas12 = document.getElementById('canvas12');
 const ctx12 = canvas12.getContext('2d');
 ctx12.lineWidth=10;ctx12.strokeStyle='coral';
@@ -136,6 +148,7 @@ ctx12.moveTo(50,300);ctx12.lineTo(550,300);
 ctx12.setLineDash([]); // 초기화 시킨다.
 ctx12.stroke();
 
+// 13.
 const canvas13 = document.getElementById('canvas13');
 const ctx13 = canvas13.getContext('2d');
 ctx13.lineWidth=20;ctx13.lineCap='round';
@@ -156,12 +169,14 @@ ctx13.moveTo(450,300);ctx13.lineTo(500,100);ctx13.lineTo(550,300);
 ctx13.lineJoin = 'bevel';
 ctx13.stroke();
 
+// 14.
 const canvas14 = document.getElementById('canvas14');
 const ctx14 = canvas14.getContext('2d');
 ctx14.fillStyle = gra3;
 ctx14.fillRect(150,100,300,200);
 ctx14.clearRect(200,150,50,50);
 
+// 15.
 const canvas15 = document.getElementById('canvas15');
 const ctx15 = canvas15.getContext('2d');
 ctx15.beginPath();
@@ -174,6 +189,7 @@ ctx15.moveTo(250,150);ctx15.lineTo(500,150);ctx15.lineTo(300,350);
 ctx15.fillStyle = gra2;
 ctx15.fill();
 
+// 16.
 const canvas16 = document.getElementById('canvas16');
 const ctx16 = canvas16.getContext('2d');
 ctx16.lineWidth=8;ctx16.lineCap='round';ctx16.lineJoin='round';
@@ -190,6 +206,7 @@ ctx16.closePath();
 ctx16.strokeStyle = 'cornflowerblue';
 ctx16.stroke();
 
+// 17.
 const canvas17 = document.getElementById('canvas17');
 const ctx17 = canvas17.getContext('2d');
 
@@ -204,6 +221,7 @@ ctx17.fillRect(0,0,600,200);
 ctx17.fillStyle= myPat1;
 ctx17.fillRect(0,200,600,200);
 
+// 18.
 const canvas18 = document.getElementById('canvas18');
 const ctx18 = canvas18.getContext('2d');
 const gra5 = ctx18.createLinearGradient(0,0,0,200);
@@ -230,6 +248,7 @@ ctx18.clip();
 ctx18.fillStyle  = gra5;
 ctx18.fillRect(0,0,600,200);
 
+// 19.
 const canvas19 = document.getElementById('canvas19');
 const ctx19 = canvas19.getContext('2d');
 ctx19.beginPath();
@@ -242,6 +261,7 @@ ctx19.setLineDash([10]);
 ctx19.strokeStyle = gra2;
 ctx19.stroke();
 
+// 20.
 const canvas20 = document.getElementById('canvas20');
 const ctx20 = canvas20.getContext('2d');
 ctx20.beginPath();
@@ -251,6 +271,7 @@ ctx20.bezierCurveTo(550,250,50,250,50,100);
 ctx20.fillStyle = gra2;
 ctx20.fill();
 
+// 21
 const canvas21 = document.getElementById('canvas21');
 const ctx21 = canvas21.getContext('2d');
 const gra21 = ctx21.createLinearGradient(0,0,canvas21.width,canvas21.height);
@@ -267,7 +288,6 @@ myImg21.onload = function(){
     ctx21.fillStyle = myPat21;
     ctx21.fillRect(0,0,600,400);
 }
-
 
 ctx21.beginPath();
 ctx21.moveTo(0,100);
@@ -290,13 +310,14 @@ ctx21.clip();
 ctx21.fillStyle = gra21;
 ctx21.fillRect(0,0,600,400);
 
-
+// 22.
 const canvas22 = document.getElementById('canvas22');
 const ctx22 = canvas22.getContext('2d');
 ctx22.beginPath();
 ctx22.arc(300,200,100,0,Math.PI*1.5);
 ctx22.stroke();
 
+// 23.
 const canvas23 = document.getElementById('canvas23');
 const ctx23 = canvas23.getContext('2d');
 ctx23.beginPath();
@@ -305,6 +326,7 @@ ctx23.arc(300,200,100,0,Math.PI*1.5,true);
 ctx23.closePath();
 ctx23.stroke();
 
+// 24.
 const canvas24 = document.getElementById('canvas24');
 const ctx24 = canvas24.getContext('2d');
 ctx24.beginPath();
@@ -335,6 +357,7 @@ ctx24.lineWidth=8 ;ctx24.lineCap='round';
 ctx24.strokeStyle=gra3;
 ctx24.stroke();
 
+// 25.
 const canvas25 = document.getElementById('canvas25');
 const ctx25 = canvas25.getContext('2d');
 ctx25.beginPath();
@@ -346,6 +369,7 @@ ctx25.lineTo(100,150);ctx25.arcTo(100,100,150,100,50);
 ctx25.lineWidth=50;ctx25.strokeStyle=gra21;
 ctx25.stroke();
 
+// 26.
 const canvas26 = document.getElementById('canvas26');
 const ctx26 = canvas26.getContext('2d');
 ctx26.rect(100,100,400,200);
@@ -354,6 +378,7 @@ if(ctx26.isPointInPath(150,150)){
     ctx26.stroke();
 }
 
+// 27.
 const canvas27 = document.getElementById('canvas27');
 const ctx27 = canvas27.getContext('2d');
 const w27 = canvas27.width;
@@ -371,6 +396,7 @@ for(let i=1;i<7;i++){
     ctx27.fill();
 }
 
+// 28.
 const canvas28 = document.getElementById('canvas28');
 const ctx28 = canvas28.getContext('2d');
 
@@ -383,6 +409,7 @@ for(let i=0;i<10;i++){
     ctx28.stroke();
 }
 
+// 29.
 const canvas29 = document.getElementById('canvas29');
 const ctx29 = canvas29.getContext('2d');
 const w29 = canvas29.width;
@@ -400,6 +427,7 @@ for(let i=0;i<5;i++){
     ctx29.fill();
 }
 
+// 30.
 const canvas30 = document.getElementById('canvas30');
 const ctx30 = canvas30.getContext('2d');
 
@@ -408,14 +436,16 @@ ctx30.scale(2,2);
 ctx30.strokeStyle='red';ctx30.strokeRect(50,50,100,100);
 ctx30.strokeStyle='green';ctx30.strokeRect(100,100,100,100);
 ctx30.scale(0.5,0.5);
-ctx30.strokeStyle='blue';ctx30.strokeRect(50,500,100,100);
+ctx30.strokeStyle='blue';ctx30.strokeRect(50,50,100,100);
 
+// 31.
 const canvas31 = document.getElementById('canvas31');
 const ctx31 = canvas31.getContext('2d');
 ctx31.rotate((Math.PI/180)*45);
 ctx31.fillStyle = 'rgba(0,0,0,.5)';
 ctx31.fillRect(250,150,100,100);
 
+// 32.
 const canvas32 = document.getElementById('canvas32');
 const ctx32 = canvas32.getContext('2d');
 
@@ -427,6 +457,7 @@ ctx32.fillStyle='coral';ctx32.fillRect(0,0,100,50);
 ctx32.translate(-100,-100);
 ctx32.fillStyle='cornflowerblue';ctx32.fillRect(100,50,100,50);
 
+// 33.
 const canvas33 = document.getElementById('canvas33');
 const ctx33 = canvas33.getContext('2d');
 
@@ -441,6 +472,7 @@ ctx33.fillStyle='bisque';ctx33.fillRect(250,150,100,100);
 ctx33.restore();ctx33.fillRect(275,175,50,50);
 ctx33.restore();ctx33.fillRect(290,190,20,20);
 
+// 34.
 const canvas34 = document.getElementById('canvas34');
 const ctx34 = canvas34.getContext('2d');
 ctx34.globalAlpha = .5;
@@ -456,3 +488,93 @@ ctx34.translate(400,200);
 ctx34.rotate(25*(Math.PI/180));
 ctx34.translate(-400,-200);
 ctx34.fillStyle='coral';ctx34.fillRect(350,150,100,100);
+
+// 35.
+const canvas35 = document.getElementById('canvas35');
+const ctx35 = canvas35.getContext('2d');
+for(let i=0;i<canvas35.height/50;i++){
+    for(let j=0;j<canvas35.width/50;j++){
+        ctx35.save();
+        ctx35.fillStyle = `rgb(${30*j}, ${255-(30*i)}, 255)`;
+        ctx35.translate(10+j*50,10+i*50);
+        ctx35.fillRect(0,0,30,30);
+        ctx35.restore();
+    }
+}
+
+// 36.
+const canvas36 = document.getElementById('canvas36');
+const ctx36 = canvas36.getContext('2d');
+
+// 37.
+const canvas37 = document.getElementById('canvas37');
+const ctx37 = canvas37.getContext('2d');
+ctx37.font = 'bold 7em Arial,sans-serif';
+ctx37.fillStyle=gra3;
+ctx37.fillText('Canvas',100,240);
+
+// 38.
+const canvas38 = document.getElementById('canvas38');
+const ctx38 = canvas38.getContext('2d');
+ctx38.font = 'bold 7em Arial,sans-serif';
+ctx38.strokeStyle=gra3;ctx38.lineWidth=5;
+ctx38.strokeText('Canvas',100,240);
+
+// 39.
+const canvas39 = document.getElementById('canvas39');
+const ctx39 = canvas39.getContext('2d');
+const x39 = canvas39.width/2;
+const y39 = canvas39.height/5-10;
+const alignText=['start','end','left','center','right'];
+
+ctx39.strokeStyle = 'red';
+ctx39.moveTo(x39,0);ctx39.lineTo(x39,canvas39.height);
+ctx39.stroke();
+ctx39.font = 'bold 20px Arial,sans-serif';
+
+for(let i=0;i<5;i++){
+    ctx39.textAlign = alignText[i];
+    ctx39.fillText(alignText[i],x39,y39*(i+1))
+}
+
+// ctx39.textAlign='start';
+// ctx39.fillText('start',x39,40);
+// ctx39.textAlign='end';
+// ctx39.fillText('end',x39,120);
+// ctx39.textAlign='left';
+// ctx39.fillText('left',x39,200);
+// ctx39.textAlign='center';
+// ctx39.fillText('center',x39,280);
+// ctx39.textAlign='right';
+// ctx39.fillText('right',x39,360);
+
+// 40.
+const canvas40 = document.getElementById('canvas40');
+const ctx40 = canvas40.getContext('2d');
+const x40 = canvas40.width/5;
+const y40 = canvas40.height/2;
+
+ctx40.strokeStyle = 'red';
+ctx40.moveTo(0,y40);ctx40.lineTo(canvas40.width,y40);
+ctx40.stroke();
+
+ctx40.font='bold 20px Arial,sans-serif';
+ctx40.textAlign='center';
+ctx40.textBaseline='top';
+ctx40.fillText('top',x40-50,y40);
+ctx40.textBaseline='bottom';
+ctx40.fillText('bottom',x40*2-50,y40);
+ctx40.textBaseline='middle';
+ctx40.fillText('middle',x40*3-50,y40);
+ctx40.textBaseline='alphabetic';
+ctx40.fillText('alphabetic',x40*4-50,y40);
+ctx40.textBaseline='hanging';
+ctx40.fillText('hanging',x40*5-50,y40);
+
+// 41.
+const canvas41 = document.getElementById('canvas41');
+const ctx41 = canvas41.getContext('2d');
+const txt41 = 'Canvas';
+ctx41.textAlign='center';
+ctx41.font='100px Arial,sans-serif';
+ctx41.fillText(txt41,canvas41.width/2,150);
